fdd2318c5e47a62ffbfe23854205f8eba9dd0a5e
robotsch <christianhumble22@gmail.com>
2022-05-04 17:58:57 -0400

    add count variables

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.application.credentials.admin[:admin_user], password: Rails.application.credentials.admin[:admin_pass]
   
   def show
+    @count = Product.count(:id)
+    @categories = Category.count(:id)
   end
 end
