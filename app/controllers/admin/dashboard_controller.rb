8959b05c62a304f0a540a5f932871bffbe90fbf6
robotsch <christianhumble22@gmail.com>
2022-05-04 17:15:41 -0400

    add admin dashboard basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  
+  http_basic_authenticate_with name: Rails.application.credentials.admin[:admin_user], password: Rails.application.credentials.admin[:admin_pass]
+  
   def show
   end
 end
