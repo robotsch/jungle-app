ae20ba3aa1c55c712bdb3807e154453d51cb1333
robotsch <christianhumble22@gmail.com>
2022-05-05 13:48:40 -0400

    add sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,9 @@ class UsersController < ApplicationController
       redirect_to '/signup'
     end
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
